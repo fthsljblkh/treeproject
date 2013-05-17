43e463c261543d068566bb97692d978a82fbb8b2
shaolun hong <shaolun.hong@gmail.com>
2013-05-17 17:40:39 +0800

    rebuild to make my life easier...

---
+++ TreeProjectRebuild/TreeProjectRebuild/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace TreeProjectRebuild
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Root world_root;
+        RootView world_view;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            world_root = new Root(GeneralLevel.Genesis);
+            world_root.BuildBranch();
+            world_view = new RootView(canvas.ActualWidth / 2, canvas.ActualHeight / 2, canvas.ActualHeight/2, world_root);
+            RootView.SetCanvas(canvas);
+            world_view.Show(GeneralLevel.Decade_Country);
+        }
+
+        /// <summary>
+        /// 在此页将要在 Frame 中显示时进行调用。
+        /// </summary>
+        /// <param name="e">描述如何访问此页的事件数据。Parameter
+        /// 属性通常用于配置页。</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
---
+++ TreeProjectRebuild/TreeProjectRebuild/RootView.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace TreeProjectRebuild
+{
+    class RootView
+    {
+        int nSides;
+        Point center;
+        double radius;  //外围圆半径
+        double initial_l;
+        double deltaR;
+        Color strokeColor, fillColor;
+        Root rootModel;
+        Ellipse borderEllipse;
+        Ellipse innerEllipse;
+        List<RootView> rootViewList;
+
+        static Canvas canvas;
+
+        internal RootView(double centerX, double centerY, double r, Root root) {  //此root即为调用Show()时作为最外层圆的root
+            center = new Point(centerX, centerY);
+            radius = r;
+            rootModel = root;
+            strokeColor = Colors.Black;
+            fillColor = Colors.Gray;
+            rootViewList = new List<RootView>();
+        }
+
+        internal void SetX(double x){
+            center.X = x;
+        }
+
+        internal void SetY(double y) {
+            center.Y = y;
+        }
+
+        internal void Show(GeneralLevel level){
+            //canvas.Children.Clear();
+            initial_l = 4;
+
+            Ellipse outerEllipse = MakeElli(center.X, center.Y, radius, strokeColor, fillColor);
+            outerEllipse.Tapped += outerEllipse_Tapped;
+            canvas.Children.Add(outerEllipse);
+
+            canvas.Children.Add(MakeElli(center.X, center.Y, 3, Colors.Black, Colors.LightGray));
+
+            if (rootModel.Level >= level)
+                return;
+
+            double[] pos = new double[2];
+            for (int i = 0; i < rootModel.branchlist.Count; ++i) {
+                double radians = 2 * Math.PI * i / rootModel.branchlist.Count;
+                Branch b = rootModel.branchlist[i];
+                double l = initial_l;
+                double r = 6;
+                for (int j = 0; j < b.rootlist.Count; ++j) {
+                    pos[0] = center.X + l * Math.Cos(radians);
+                    pos[1] = center.Y + l * Math.Sin(radians);
+
+                    RootView subRoot = new RootView(pos[0], pos[1], r, b.rootlist[j]);
+                    subRoot.Show(level);
+                    rootViewList.Add(subRoot);
+
+                    r += 0.4 * r;
+                    l += 0.8 * r;
+                    radians += (Math.PI / 180) * 20;
+                    pos[0] = center.X + l * Math.Cos(radians);
+                    pos[1] = center.Y + l * Math.Sin(radians);
+                }
+            }
+        }
+
+        void outerEllipse_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Ellipse elli = sender as Ellipse;
+            elli.Stroke = new SolidColorBrush(Colors.Red);
+        }
+
+        Ellipse MakeElli(double centerX, double centerY, double r, Color strokeColor, Color fillColor)
+        {
+            Ellipse e = new Ellipse
+            {
+                Width = r * 2,
+                Height = r * 2,
+                Stroke = new SolidColorBrush(strokeColor),
+                Fill = new SolidColorBrush(fillColor)
+            };
+            Canvas.SetLeft(e, centerX - r);
+            Canvas.SetTop(e, centerY - r);
+            return e;
+        }
+
+        internal static void SetCanvas(Canvas canvas){
+            RootView.canvas = canvas;
+        }
+    }
+}
