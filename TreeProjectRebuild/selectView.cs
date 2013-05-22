4f323c91ff6e5814080103397b5de20982b448f7
violetzijing <violet@violet-debian>
2013-05-22 15:04:07 +0800

    add control function

---
+++ TreeProjectRebuild/selectView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeProjectRebuild
+{
+    class SelectView
+    {
+        Select selectModel;
+        internal List<RootView> selectViewList;
+        internal SelectView(Select selectItem) {
+            selectModel = selectItem;
+            selectViewList = new List<RootView>();
+        }
+        internal RootView getRootView(int i) {
+            return selectViewList[i];
+        }
+
+        internal void addRootView(RootView rv) {
+		private void get_key(object sender, KeyEventArgs e)
+		{
+			if （e.KeyValue == control)
+			{
+			//message.Show("是否获得ctrl");
+			//获取圆座标
+			
+				selectViewList.add(rv);	//获得一个焦点添加一个对象
+			}
+	        }
+        }
+
+        internal int Count {
+            get {
+                return selectViewList.Count;
+            }
+        }
+
+        
+    }
+}
