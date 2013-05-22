6607908d586b97fa2f98eb6eacfdcc61a6c1c204
shaolun hong <shaolun.hong@gmail.com>
2013-05-22 19:56:50 +0800

    yeah!

---
+++ TreeProjectRebuild/TreeProjectRebuild/TP_Data.cs
@@ -0,0 +1,11 @@
+﻿class TP_Data {
+    public sbyte decade;
+    public sbyte year;
+    public sbyte season;
+
+    public sbyte country;
+    public sbyte province;
+    public sbyte city;
+
+    public double gdp;
+}
